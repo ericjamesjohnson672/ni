09d1e130f713e7189bac33372e928fc0332fcd3d
Eric Johnson <ejohnson@corp.promodel.com>
2016-10-17 09:54:04 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strinct';
+
+        angular
+            .module('app', [])
+            .controller('HomeController', HomeController);
+
+        HomeController.$inject = ['$log'];
+        function HomeController($log) {
+            var vm = this;
+            vm.title="Hello World";
+            
+            activate();;
+
+            function activate(){}
+        }
+})();
